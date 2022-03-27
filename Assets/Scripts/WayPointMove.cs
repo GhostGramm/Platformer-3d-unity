6c4d474fa1f11df6eb6dc56ace0330f82efdb549
marshalonah <marshalonah@gmail.com>
2022-03-28 00:01:37 +0100

    Moving floor implemented

--- Assets/Scripts/WayPointMove.cs
+++ Assets/Scripts/WayPointMove.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class WayPointMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform[] Locations;
+    public float Speed = 1f;
+
+    private int CurrentLocation = 0;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveGameObject();
+    }
+
+    void MoveGameObject()
+    {
+        if (Vector3.Distance(transform.position, Locations[CurrentLocation].position) < 0.1f)
+            CurrentLocation++;
+
+        if (CurrentLocation >= Locations.Length)
+            CurrentLocation = 0;
+
+        transform.position = Vector3.MoveTowards(transform.position, Locations[CurrentLocation].position, Speed * Time.deltaTime);
+
     }
 }
