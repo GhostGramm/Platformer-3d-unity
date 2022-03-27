d7f07872e1e8f7345ede9fc641d843c5096572b1
marshalonah <marshalonah@gmail.com>
2022-03-27 16:07:06 +0100

    simple movement implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float moveForce = 5f;
+    private float jumpForce = 5f;
     void Start()
     {
-        Debug.Log("initial commit");
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Movement();
+    }
 
+    void Movement()
+    {
+        if (Input.GetKeyDown("space"))
+            GetComponent<Rigidbody>().velocity = new Vector3(0, jumpForce, 0);
+
+        if (Input.GetKeyDown("up"))
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, moveForce);
+
+        if (Input.GetKeyDown("down"))
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -moveForce);
+
+        if (Input.GetKeyDown("left"))
+            GetComponent<Rigidbody>().velocity = new Vector3(-moveForce, 0, 0);
+
+        if (Input.GetKeyDown("right"))
+            GetComponent<Rigidbody>().velocity = new Vector3(moveForce, 0, 0);
     }
 }
